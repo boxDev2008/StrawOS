140ac41bc2f1ebb73b37a2758db2c1f9a87b77de
Kiril Abadjiev <kikoabadjiev@gmail.com>
2026-03-17 15:37:14 +0200

    fixed a few issues

--- modules/lib/libc/include/unistd.h
+++ modules/lib/libc/include/unistd.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <stat.h>
 
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
 #define O_RDONLY    0x000
 #define O_WRONLY    0x001
 #define O_RDWR      0x002
